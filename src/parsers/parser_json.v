eeba902eb2397c0d949160942a7a0c67260ccd78
André Luiz <andre_jp97@hotmail.com>
2023-06-23 19:23:33 -0300

    resolver types from values

--- src/parsers/parser_json.v
+++ src/parsers/parser_json.v
@@ -24,6 +24,7 @@ fn resolver_key_value(obj_json json2.Any, mut struct_obj_json ObjStruct) {
 	for key, value in obj_json.as_map() {
 		if value is []json2.Any {
 			mut children := ObjStruct{
+				name: key
 				typ: .array
 			}
 			resolver_array(value, mut children)
@@ -45,20 +46,22 @@ fn resolver_array(obj_json json2.Any, mut struct_obj_json ObjStruct) {
 		for key, value in item.as_map() {
 			if value is []json2.Any {
 				mut children := ObjStruct{
+					name: key
 					typ: .array
 				}
 				resolver_array(value, mut children)
 				struct_obj_json.children << children
 			} else if value is map[string]json2.Any {
 				mut children := ObjStruct{
+					name: key
 					typ: .object
 				}
 				resolver_key_value(value, mut children)
 				struct_obj_json.children << children
 			} else {
+				t := resolver_type(value)
 				struct_obj_json.children << ObjStruct{
-					name: key
-					typ: resolver_type(value)
+					typ: t
 					value: contracts.Any(value.str())
 				}
 			}
@@ -67,5 +70,14 @@ fn resolver_array(obj_json json2.Any, mut struct_obj_json ObjStruct) {
 }
 
 fn resolver_type(value json2.Any) ObjType {
+	if value is bool {
+		return .bool
+	} else if value is f64 || value is i64 {
+		return .number
+	} else if value is map[string]json2.Any {
+		return .object
+	} else if value is []json2.Any {
+		return .array
+	}
 	return .string
 }
